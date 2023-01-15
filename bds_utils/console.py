fd89008478eda89ac814ef08daa7618545b23b2d
Dan Ford <dan@danford.dev>
2023-01-15 21:55:00 +0000

    Console using cmd module

--- bds_utils/console.py
+++ bds_utils/console.py
@@ -1,18 +1,33 @@
+import cmd
 import sys
 import xmlrpc.client
 
-class BedrockServerConsole:
+class BedrockServerConsole(cmd.Cmd):
+    intro = 'Type help or ? to list commands.\n'
+    prompt = 'bds> '
+    file = None
 
     SERVER_URL = 'http://localhost:11542'
     COMMAND = ' '.join(sys.argv[1:])
 
     def __init__(self, server_url=SERVER_URL) -> None:
+        super().__init__()
         self.server = xmlrpc.client.ServerProxy(server_url)
 
-    def send_command(self, command_str):
+    def do_cmd(self, command_str):
+        'Send command to server: CMD <command string>'
+
         print("Sending command: '%s' to RPC server: %s" % (command_str, self.server))
         self.server.send_command(command_str)
 
+    def do_exit(self, arg):
+        'Exit the console'
+
+        return True
+
 def run_console():
-    BedrockServerConsole().send_command(' '.join(sys.argv[1:]))
+    if len(sys.argv) > 1:
+        BedrockServerConsole(' '.join(sys.argv[1:])).cmdloop()
+    else:
+        BedrockServerConsole().cmdloop()
 
